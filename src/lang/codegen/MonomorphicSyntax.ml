3a0706b3c658ab8dee076680e1708cc6c3a8ff26
Vaivaswatha N <vaivaswatha@users.noreply.github.com>
2019-05-06 13:33:35 +0530

    Add a pass to instantiate all type polymorphic functions (#4)
    * add two non-trivial tests for monomorphize
    
    * Add AST for monomorphic program
    
    * move directory
    
    * add scilla-codegen.opam for dune builds
    
    * Add a basic analysis computing all instantiations and a dummy transformation.
    
    * compute instantiations and do all substitutions
    
    * make add_tapp function more general
    
    * Compute all instantiations of all TFuns
    
    * fix typos in comment
    
    * Upgrade monomorphizer to new syntax with libtree
    
    * Add a runner scilla-compiler
    
    * Add a runner for compiling pure expressions
    
    * Simplify the monomorphize algorithm, but a little less efficient.
    
    The monormorphize algorithm previously did a little better than
    "all instantiations flow into all type functions", by keeping track
    of actual type applications and eliminating some based on whether
    a sequence of ground types fits into a sequence of type functions.
    This seems unnecessarily complicated, so remove it. We are now
    back to "all instantiations flow into all type functions".
    
    * Add tests as part of testsuite
    
    * rename function based on review comment

---
+++ src/lang/codegen/MonomorphicSyntax.ml
@@ -0,0 +1,133 @@
+(*
+  This file is part of scilla.
+
+  Copyright (c) 2019 - present Zilliqa Research Pvt. Ltd.
+
+  scilla is free software: you can redistribute it and/or modify it under the
+  terms of the GNU General Public License as published by the Free Software
+  Foundation, either version 3 of the License, or (at your option) any later
+  version.
+
+  scilla is distributed in the hope that it will be useful, but WITHOUT ANY
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+  A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along with
+*)
+
+open Syntax
+
+(* Scilla AST without parametric polymorphism. *)
+module MmphSyntax (SR : Rep) (ER : Rep) = struct
+  (* Syntax reference: http://gallium.inria.fr/blog/overriding-submodules/ *)
+  include (ScillaSyntax(SR)(ER) :
+    (* We want all definitions from ScillaSyntax except for expr. *)
+    module type of ScillaSyntax(SR)(ER) with
+      type expr_annot := ScillaSyntax(SR)(ER).expr_annot and
+      type expr := ScillaSyntax(SR)(ER).expr and
+      type stmt_annot := ScillaSyntax(SR)(ER).stmt_annot and
+      type stmt := ScillaSyntax(SR)(ER).stmt and
+      type component := ScillaSyntax(SR)(ER).component and
+      type ctr_def := ScillaSyntax(SR)(ER).ctr_def and
+      type lib_entry := ScillaSyntax(SR)(ER).lib_entry and
+      type library := ScillaSyntax(SR)(ER).library and
+      type contract := ScillaSyntax(SR)(ER).contract and
+      type cmodule := ScillaSyntax(SR)(ER).cmodule and
+      type lmodule := ScillaSyntax(SR)(ER).lmodule and
+      type libtree := ScillaSyntax(SR)(ER).libtree
+  )
+
+  (* This is identical to ScillaSyntax.expr except for
+   *  - TFun replaced with TFunMap.
+   *  - TApp replaced with TFunSel.
+   *)
+  type expr_annot = expr * ER.rep
+  and expr =
+    | Literal of literal
+    | Var of ER.rep ident
+    | Let of ER.rep ident * typ option * expr_annot * expr_annot
+    | Message of (string * payload) list
+    | Fun of ER.rep ident * typ * expr_annot
+    | App of ER.rep ident * ER.rep ident list
+    | Constr of string * typ list * ER.rep ident list
+    | MatchExpr of ER.rep ident * (pattern * expr_annot) list
+    | Builtin of ER.rep builtin_annot * ER.rep ident list
+    (* Rather than one polymorphic function, we have expr for each instantiated type. *)
+    (* The original polymorphic function is retained only for convenience *)
+    | TFunMap of (ER.rep ident * expr_annot) * (typ * expr_annot) list
+    (* Select an already instantiated expression of id based on the typ.
+     * It is expected that id resolves to a TFunMap. *)
+    | TFunSel of ER.rep ident * typ list
+    (* Fixpoint combinator: used to implement recursion principles *)
+    | Fixpoint of ER.rep ident * typ * expr_annot
+
+    (***************************************************************)
+    (* All definions below are identical to the ones in Syntax.ml. *)
+    (***************************************************************)
+
+    type stmt_annot = stmt * SR.rep
+    and stmt =
+      | Load of ER.rep ident * ER.rep ident
+      | Store of ER.rep ident * ER.rep ident
+      | Bind of ER.rep ident * expr_annot
+      (* m[k1][k2][..] := v OR delete m[k1][k2][...] *)
+      | MapUpdate of ER.rep ident * (ER.rep ident list) * ER.rep ident option
+      (* v <- m[k1][k2][...] OR b <- exists m[k1][k2][...] *)
+      (* If the bool is set, then we interpret this as value retrieve, 
+         otherwise as an "exists" query. *)
+      | MapGet of ER.rep ident * ER.rep ident * (ER.rep ident list) * bool
+      | MatchStmt of ER.rep ident * (pattern * stmt_annot list) list
+      | ReadFromBC of ER.rep ident * string
+      | AcceptPayment
+      | SendMsgs of ER.rep ident
+      | CreateEvnt of ER.rep ident
+      | Throw of ER.rep ident
+
+  type component =
+    { comp_type   : component_type;
+      comp_name   : SR.rep ident;
+      comp_params : (ER.rep ident * typ) list;
+      comp_body   : stmt_annot list }
+
+    type ctr_def =
+      { cname : ER.rep ident; c_arg_types : typ list }
+    
+    type lib_entry =
+      | LibVar of ER.rep ident * expr_annot
+      | LibTyp of ER.rep ident * ctr_def list
+  
+    type library =
+      { lname : SR.rep ident;
+        lentries : lib_entry list }
+    
+    type contract =
+      { cname   : SR.rep ident;
+        cparams : (ER.rep ident  * typ) list;
+        cfields : (ER.rep ident * typ * expr_annot) list;
+        ccomps  : component list; }
+  
+    (* Contract module: libary + contract definiton *)
+    type cmodule =
+      { smver : int;                (* Scilla major version of the contract. *)
+        cname : SR.rep ident;
+        libs  : library option;     (* lib functions defined in the module *)
+      (* List of imports / external libs with an optional namespace. *)
+        elibs : (SR.rep ident * SR.rep ident option) list;
+        contr : contract }
+
+    (* Library module *)
+    type lmodule =
+      {
+        (* List of imports / external libs with an optional namespace. *)
+        elibs : (SR.rep ident * SR.rep ident option) list;
+        libs : library; (* lib functions defined in the module *)
+      }
+
+    (* A tree of libraries linked to their dependents *)
+    type libtree =
+      {
+        libn : library;      (* The library this node represents *)
+        deps : libtree list  (* List of dependent libraries *)
+      }
+
+end
---
+++ src/lang/codegen/Monomorphize.ml
@@ -0,0 +1,473 @@
+(*
+  This file is part of scilla.
+
+  Copyright (c) 2019 - present Zilliqa Research Pvt. Ltd.
+  
+  scilla is free software: you can redistribute it and/or modify it under the
+  terms of the GNU General Public License as published by the Free Software
+  Foundation, either version 3 of the License, or (at your option) any later
+  version.
+ 
+  scilla is distributed in the hope that it will be useful, but WITHOUT ANY
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+  A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ 
+  You should have received a copy of the GNU General Public License along with
+*)
+
+
+(* 
+  * This file contains code to instantiate all type polymorphic functions
+  * in a Scilla program. The new AST is a variation of ScillaSyntax which
+  * replace TApp and TFun expressions with TFunSel and TFunMap expressions.
+  * Postponing the analysis of computing the possible instantiations of a
+  * TFun expression, we make a naive assumption that all types used in TApps
+  * in the whole program can be used to instantiate a TFun. While this is
+  * conservative (and imprecise), it is safe.
+
+  * - `analyze_module` is the entry to the analysis that goes through the AST and
+  * computes a list of type applications. This is `type list`. It is a simple
+  * walk of the AST looking for `TApp` expressions and adding to the list (if the
+  * list doesn't already contain an identical `TApp`). By way of this being a
+  * simple walk, it does not filter out non-ground-type `TApp`s (instantiations).
+  * - As a post-process on the analyzed data from `analyze_module`, to eliminate
+  * presence of non-ground types, `postprocess_tappl` is called, which does:
+  *    * Build a list of ground types scanning through the tapps list from
+         `analyze_module`.
+       * This finite list of ground types is substituted into all non-ground
+         types in the tapps list, in all combinations possible.
+       * The functions to do this are written in a bottom-up fashion, where
+         functions that do the smallest of substitutions are defined first and 
+         these are then used to substitute at a higher abstract. In each level
+         here, there is a potential exponential blow-up of substituted ground
+         types.
+
+  * Finally, the output of `postprocess_tappl`, a list of ground types,
+    is used to instantiate the input program. For each `TFun` expression, the
+    function `compute_instantiations` uses the tapps data to compute (based on
+    the heuristic described above) the possible instantiations.
+
+  * TODO: (1) A more precise analysis to compute possible instantiations of a TFun
+  * exprsesion. (2) Update / add annotations to instantiated expressions.
+*)
+
+
+open TypeUtil
+open Syntax
+open MonadUtil
+open Core.Result.Let_syntax
+open MonomorphicSyntax
+
+(* Translate ScillaSyntax to MonomorphicSyntax. *)
+module ScillaCG_Mmph
+    (SR : Rep)
+    (ER : sig
+       include Rep
+       val get_type : rep -> PlainTypes.t inferred_type
+     end) = struct
+
+  module SER = SR
+  module EER = ER
+  module TU = TypeUtilities (SR) (ER)
+  module TypedSyntax = ScillaSyntax (SR) (ER)
+  module MS = MmphSyntax (SR) (ER)
+
+  open TypedSyntax
+
+  (*******************************************************)
+  (* Gather possible instances of polymorphic functions  *)
+  (*******************************************************)
+
+  (* TODO: More precise analysis:
+   *       "A Type- and Control-Flow Analysis for System F Technical Report" - Matthew Fluet. 
+   * Currently only have "all TApps flow to all TFuns". *)
+
+  (* Add a type application to tracked list of instantiations. If any type has a
+   * free TypeVar that is not in bound_tvars, return error. If the TypeVar
+   * is bound, then it is tracked in the env as PolyFun(TypeVar) to make
+   * simple to compare types using type_equiv (alpha equivalence). *)
+  let add_tapp tenv tapp bound_tvars lc =
+    let%bind tapp' = mapM ~f:(fun t ->
+      let ftvs = free_tvars t in
+      match List.find_opt (fun v -> not (List.mem v bound_tvars)) ftvs with
+      | Some v -> fail1 (Printf.sprintf "Monomorphize: Unbound type variable %s used in instantiation" v) lc
+      | None ->
+        (* Bind all free variables for it to work with type_equiv *)
+        pure @@ List.fold_left (fun acc ftv -> PolyFun (ftv, acc)) t ftvs
+      ) tapp in
+    (* For each type in tapp', if doesn't exist in tenv, add it. *)
+    pure @@ 
+      List.fold_left (fun accenv t ->
+        Utils.list_add_unique ~equal:(TU.type_equiv) accenv t
+      ) tenv tapp'
+
+  (* Walk through "e" and add all TApps. *)
+  let rec analyse_expr (e, erep) tenv (bound_tvars : string list) =
+  match e with
+  | Literal _ | Var _ |  Message _ | App _ 
+  | Constr _ | Builtin _  -> pure tenv
+  | Fixpoint (_, _, body) | Fun (_, _, body) -> analyse_expr body tenv bound_tvars
+  | Let (_, _, lhs, rhs) ->
+    let%bind tenv' = analyse_expr lhs tenv bound_tvars in
+    analyse_expr rhs tenv' bound_tvars
+  | MatchExpr (_, clauses) ->
+    foldM ~f:(fun accenv (_, bre) ->
+      let%bind tenv' = analyse_expr bre accenv bound_tvars in
+      pure tenv'
+    ) ~init:tenv clauses
+  | TFun (v, e') -> analyse_expr e' tenv ((get_id v) :: bound_tvars)
+  | TApp (_, tapp) -> add_tapp tenv tapp bound_tvars (ER.get_loc erep)
+
+  (* Walk through statement list and add all TApps. *)
+  let rec analyse_stmts stmts tenv =
+  match stmts with
+  | [] -> pure tenv
+  | (s, _) :: sts ->
+    (match s with
+    | Load _ | Store _ | MapUpdate _ | MapGet _ | ReadFromBC _
+    | AcceptPayment | SendMsgs _ | CreateEvnt _ | Throw _ ->
+      analyse_stmts sts tenv
+    | Bind (_ , e) ->
+      let%bind tenv' = analyse_expr e tenv [] in
+      analyse_stmts sts tenv'
+    | MatchStmt (_, pslist) ->
+      foldM ~f:(fun accenv (_, slist) ->
+        let%bind tenv' = analyse_stmts slist accenv in
+        pure tenv'
+      ) ~init:tenv pslist
+    )
+
+  (* Walk through entire module, tracking TApps. *)
+  let analyze_module (cmod : cmodule) rlibs elibs =
+
+    (* Function to anaylze library entries. *)
+    let analyze_lib_entries env lentries =
+      foldM ~f:(fun accenv lentry ->
+        match lentry with
+        | LibVar (_, lexp) ->
+          let%bind tenv' = analyse_expr lexp accenv [] in
+          pure tenv'
+        | LibTyp _ -> pure accenv
+      ) ~init:env lentries
+    in
+
+    (* Analyze recursion library entries. *)
+    let%bind rlib_env = analyze_lib_entries [] rlibs in
+
+    (* Function to analyze external and contract libraries. *)
+    let analyze_library env lib = analyze_lib_entries env lib.lentries in
+
+    (* Analyze full library tree. *)
+    let rec analyze_libtree env lib =
+      (* first analyze all the dependent libraries. *)
+      let%bind env' = foldM ~f:(fun accenv lib ->
+        analyze_libtree accenv lib
+      ) ~init:env lib.deps in
+      (* analyze this library. *)
+      analyze_library env' lib.libn
+    in
+    let%bind elibs_env =
+      foldM ~f:(fun accenv libt ->
+        analyze_libtree accenv libt
+      ) ~init: rlib_env elibs
+    in
+    let%bind libs_env = match cmod.libs with
+      | Some lib -> analyze_library elibs_env lib
+      | None -> pure elibs_env
+    in
+
+    (* Analyze fields. *)
+    let%bind fields_env = foldM ~f:(fun accenv (_, _, fexp) ->
+      let%bind tenv' = analyse_expr fexp accenv [] in
+      pure tenv'
+    ) ~init:libs_env cmod.contr.cfields
+    in
+
+    (* Analyze transitions. *)
+    let%bind trans_env = foldM ~f:(fun accenv comp ->
+      let%bind tenv' = analyse_stmts comp.comp_body accenv in
+      pure tenv'
+    ) ~init:fields_env cmod.contr.ccomps
+    in
+    pure trans_env
+
+  (* The type applications collected by analyze_module may have non-ground applications.
+   * This function will substitute into and eliminate such applications. *)
+  let postprocess_tappl tappl =
+
+    (* Get a list of all ground types. *)
+    let gts =
+      List.fold_left (fun acc t ->
+        if TU.is_ground_type t
+        then
+          (* If "t" is not already in acc, add it. *)
+          Utils.list_add_unique ~equal:(TU.type_equiv) acc t
+        else
+          acc
+      ) [] tappl
+    in
+
+    (* Given a type, substitute all of the given ground types, in all combinations. 
+     * Return a list of concrete types that will replace the input type. *)
+    let eliminate_tvars t gts =
+      (* First ensure that all TVars are bound. *)
+      if free_tvars t <> [] then fail0 "Unbound type variables during Monomorphize" else
+
+      (* Subsitute the first PolyFun in t with tg. *)
+      let subst_first_polyfun t tg =
+        let rec subst t =
+          (* Call subst for each element in tls, till there is a substitution. *)
+          let subst_tlist tls =
+            let%bind (_, rtls) = foldM ~f:(fun (substituted, rtls) t ->
+              if substituted then pure (true, t :: rtls) else
+              let%bind t' = subst t in
+              pure (t = t', t'::rtls)
+            ) ~init:(false, []) tls 
+            in
+            pure @@ List.rev rtls
+          in
+          match t with
+          | PrimType _ | Unit -> pure t
+          | MapType (kt, vt) ->
+            let%bind s' = subst_tlist [kt;vt] in
+            (match s' with
+            | [kt';vt'] -> pure @@ MapType(kt', vt')
+            | _ -> fail0 "Monomorphize: Internal error in type substitution")
+          | FunType  (at, ft) ->
+            let%bind s' = subst_tlist [at;ft] in
+            (match s' with
+            | [kt';vt'] -> pure @@ FunType(kt', vt')
+            | _ -> fail0 "Monomorphize: Internal error in type substitution")
+          | ADT (tname, tls) ->
+            let%bind tls' = subst_tlist tls in
+            pure @@ ADT(tname, tls')
+          | TypeVar tv -> fail0 (Printf.sprintf "Monomorphize: Unbound TypeVar %s" tv)
+          | PolyFun (tv, tbody) ->
+            (* replace tv with tg in tbody. *)
+            pure @@ subst_type_in_type tv tg tbody
+        in
+        subst t
+      in
+
+      (* Substitute each ground type in tgs into all types in ts. 
+      * The result is a list of size length(tgs) * length(ts). *)
+      let subst_all_ground_into_tlist ts tgs =
+        let%bind ts' = mapM ~f:(fun t ->
+          mapM ~f:(fun tg ->
+            subst_first_polyfun t tg
+          ) tgs
+        ) ts
+        in
+        pure @@ List.concat ts'
+      in
+
+      (* Call "subst_all_ground_into_tlist" till no more substitutions are possible. *)
+      let rec eliminate ts tgs =
+        match ts with
+        | [] -> pure ts
+        | t :: _ -> (* Check just one element, others are expected to be the same. *)
+          (* if "t" is ground type, no more substitutions are possible. *)
+          if TU.is_ground_type t then pure ts else
+          let%bind ts' = subst_all_ground_into_tlist ts tgs in
+          eliminate ts' tgs
+      in
+
+      (* We end the definition of eliminate_tvars by calling eliminate. *)
+      eliminate [t] gts
+    in
+
+    (* We now have a list of ground types for substitution into tappl 
+     * AND the tool to do the substitution in all possible ways and return
+     * a list of substituted types. We need to do this substitution into
+     * all types in the input list of type applications: tappl. *)
+
+    foldM ~f:(fun acc ta ->
+      let%bind tgs = eliminate_tvars ta gts in
+      (* Add-unique each t in tgs to acc. *)
+      let acc' = List.fold_left (fun acc t ->
+        Utils.list_add_unique ~equal:(TU.type_equiv) acc t
+      ) acc tgs in
+      pure acc'
+    ) ~init:[] tappl
+  (* End of postprocess_tappl. *)
+
+  (* Walk through "e" and replace TFun and TApp with TFunMap and TFunSel respectively. *)
+  let rec monomorphize_expr (e, erep) tappl =
+    match e with
+    | Literal l -> pure ((MS.Literal l), erep)
+    | Var v -> pure ((MS.Var v), erep)
+    | Message m -> pure ((MS.Message m), erep)
+    | App (a, l) -> pure ((MS.App (a, l)), erep)
+    | Constr (s, tl, il) -> pure ((MS.Constr (s, tl, il)), erep)
+    | Builtin (i, il)  -> pure ((MS.Builtin (i, il)), erep)
+    | Fixpoint (i, t, body) ->
+      let%bind body' = monomorphize_expr body tappl in
+      pure ((MS.Fixpoint (i, t, body')), erep)
+    | Fun (i, t, body) ->
+      let%bind body' = monomorphize_expr body tappl in
+      pure ((MS.Fun (i, t, body')), erep)
+    | Let (i, topt, lhs, rhs) ->
+      let%bind lhs' = monomorphize_expr lhs tappl in
+      let%bind rhs' = monomorphize_expr rhs tappl in
+      pure ((MS.Let (i, topt, lhs', rhs')), erep)
+    | MatchExpr (i, clauses) ->
+      let%bind clauses' = mapM ~f:(fun (p, cexp) ->
+        let%bind cexp' = monomorphize_expr cexp tappl in
+        pure (p, cexp')
+      ) clauses
+      in
+      pure (MS.MatchExpr(i, clauses'), erep)
+    | TFun (v, body) ->
+      let%bind body' = monomorphize_expr body tappl in
+      let%bind tfuns = mapM ~f:(fun t ->
+        if (free_tvars t) <> [] || not (TU.is_ground_type t)
+        then
+          fail1 "Internal error. Attempting to instantiate with a non-ground type or type variable." (ER.get_loc erep)
+        else
+          (* ******************************************************************************* *)
+          let lc = ER.get_loc erep in
+          Printf.printf "Instantiating at (%s,%d,%d) with type: %s\n" lc.fname lc.lnum lc.cnum (pp_typ t);
+          (* ******************************************************************************* *)
+          let ibody = MS.subst_type_in_expr v t body in
+          let%bind ibody' = monomorphize_expr ibody tappl in
+          pure (t, ibody')
+      ) tappl in
+      pure ((MS.TFunMap ((v, body'), tfuns)), erep)
+    | TApp (i, tl) -> pure ((MS.TFunSel (i, tl)), erep)
+  
+    (* Walk through statement list and replace TFun and TApp with TFunMap and TFunSel respectively. *)
+  let rec monomorphize_stmts stmts tappl : ((MS.stmt_annot list), 'a) result =
+    match stmts with
+    | [] -> pure []
+    | (s, srep) :: sts ->
+      let%bind sts' = monomorphize_stmts sts tappl in
+      (match s with
+      | Load (x, m) ->
+        let s' = MS.Load(x, m) in
+        pure ((s', srep) :: sts')
+      | Store (m, i) ->
+        let s' = MS.Store(m, i) in
+        pure ((s', srep) :: sts')
+      | MapUpdate (i, il, io) ->
+        let s' = MS.MapUpdate (i, il, io) in
+        pure ((s', srep) :: sts')
+      | MapGet (i, i', il, b) ->
+        let s' = MS.MapGet (i, i', il, b) in
+        pure ((s', srep) :: sts')
+      | ReadFromBC (i, s) ->
+        let s' = MS.ReadFromBC (i, s) in
+        pure ((s', srep) :: sts')
+      | AcceptPayment ->
+        let s' = MS.AcceptPayment in
+        pure ((s', srep) :: sts')
+      | SendMsgs m ->
+        let s' = MS.SendMsgs(m) in
+        pure ((s', srep) :: sts')
+      | CreateEvnt e ->
+        let s' = MS.CreateEvnt(e) in
+        pure ((s', srep) :: sts') 
+      | Throw t ->
+        let s' = MS.Throw(t) in
+        pure ((s', srep) :: sts')
+      | Bind (i , e) ->
+        let%bind e' = monomorphize_expr e tappl in
+        let s' = MS.Bind(i, e') in
+        pure ((s', srep) :: sts')
+      | MatchStmt (i, pslist) ->
+        let%bind pslist' = mapM ~f:(fun (p, ss) ->
+          let%bind ss' = monomorphize_stmts ss tappl in
+          pure(p, ss')
+        ) pslist
+        in
+        let s' = MS.MatchStmt(i, pslist') in
+        pure ((s', srep) :: sts')
+      )
+  
+  (* Walk through entire module and replace TFun and TApp with TFunMap and TFunSel respectively. *)
+  let monomorphize_module (cmod : cmodule) rlibs elibs =
+    (* Analyze and find all possible instantiations. *)
+    let%bind tappl' = analyze_module cmod rlibs elibs in
+    let%bind tappl = postprocess_tappl tappl' in
+
+    (* Function to monomorphize library entries. *)
+    let monomorphize_lib_entries tappl lentries =
+      mapM ~f:(fun lentry ->
+        match lentry with
+        | LibVar (i, lexp) ->
+          let%bind lexp' = monomorphize_expr lexp tappl in
+          pure (MS.LibVar(i, lexp'))
+        | LibTyp (i, tdefs) ->
+          let tdefs' = List.map (fun (t : ctr_def) ->
+            { MS.cname = t.cname; MS.c_arg_types = t.c_arg_types }
+          ) tdefs in
+          pure (MS.LibTyp (i, tdefs'))
+      ) lentries
+    in
+
+    (* Translate recursion libs. *)
+    let%bind rlibs' = monomorphize_lib_entries tappl rlibs
+    in
+
+    (* Function to monomorphize a library. *)
+    let monomorphize_lib tappl lib =
+      let%bind lentries' = monomorphize_lib_entries tappl lib.lentries in
+      let lib' = { MS.lname = lib.lname; lentries = lentries' } in
+      pure lib'
+    in
+
+    (* Monomorphize the library tree. *)
+    let rec monomorphize_libtree tappl libt =
+      let%bind deps' = mapM ~f:(fun dep ->
+        monomorphize_libtree tappl dep
+      ) libt.deps in
+      let%bind libn' = monomorphize_lib tappl libt.libn in
+      let libt' = { MS.libn = libn'; MS.deps = deps' } in
+      pure libt'
+    in
+    let%bind elibs' = mapM ~f:(fun elib ->
+      monomorphize_libtree tappl elib
+    ) elibs in
+
+    (* Translate contract library. *)
+    let%bind clibs' = match cmod.libs with
+      | Some clib ->
+        let%bind clib' = monomorphize_lib tappl clib in
+        pure @@ Some (clib')
+      | None -> pure None
+    in
+
+    (* Translate fields and their initializations. *)
+    let%bind fields' = mapM ~f:(fun (i, t, fexp) ->
+      let%bind fexp' = monomorphize_expr fexp tappl in
+      pure (i, t, fexp')
+    ) cmod.contr.cfields
+    in
+
+    (* Translate all contract components. *)
+    let%bind comps' = mapM ~f:(fun comp ->
+      let%bind body' = monomorphize_stmts comp.comp_body tappl in
+      pure { MS.comp_type = comp.comp_type; MS.comp_name = comp.comp_name; MS.comp_params = comp.comp_params; MS.comp_body = body' }
+    ) cmod.contr.ccomps
+    in
+
+    let contr' = { MS.cname = cmod.contr.cname; MS.cparams = cmod.contr.cparams;
+                   MS.cfields = fields'; ccomps = comps' } in
+    let cmod' = { MS.smver = cmod.smver; MS.cname = cmod.cname;
+                  MS.elibs = cmod.elibs;
+                  MS.libs = clibs'; MS.contr = contr' } in
+
+    (* Return back the whole program, transformed. *)
+    pure (cmod', rlibs', elibs')
+
+  (* For monomorphizing standalone expressions. *)
+  let monomorphize_expr_wrapper expr =
+    let%bind tappl' = analyse_expr expr [] [] in
+    let%bind tappl = postprocess_tappl tappl' in
+    let%bind expr' = monomorphize_expr expr tappl in
+    pure expr'
+
+  module OutputSyntax = MS
+  module OutputSRep = SER
+  module OutputERep = EER
+
+end
