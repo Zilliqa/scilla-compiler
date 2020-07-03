336390896235b1022a71b67c6f2a0af8350e511c
Vaivaswatha Nagaraj <vaivaswatha@zilliqa.com>
2020-07-03 12:09:14 +0530

    Add ackerman(3,7) test

--- tests/codegen/expr/TestCodegenExpr.ml
+++ tests/codegen/expr/TestCodegenExpr.ml
@@ -26,6 +26,7 @@ let explist =
     "dce1.scilexp";
     "fib.scilexp";
     "ackermann.scilexp";
+    "ackermann_3_7.scilexp";
     "typ-inst.scilexp";
     "typ1-inst.scilexp";
     "typ2-inst.scilexp";
