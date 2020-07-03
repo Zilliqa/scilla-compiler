00b4f41c1cc2d110c5307d3d9449abaa30763075
Vaivaswatha Nagaraj <vaivaswatha@zilliqa.com>
2020-07-03 18:11:53 +0530

    The second version of church_nat is not STLC. Rename files.

--- tests/codegen/expr/TestCodegenExpr.ml
+++ tests/codegen/expr/TestCodegenExpr.ml
@@ -28,7 +28,7 @@ let explist =
     "ackermann.scilexp";
     "ackermann_3_7.scilexp";
     "church_nat_stlc.scilexp";
-    "church_nat_stlc_2.scilexp";
+    "church_nat_2.scilexp";
     "typ-inst.scilexp";
     "typ1-inst.scilexp";
     "typ2-inst.scilexp";
