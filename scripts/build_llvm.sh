586b9de58dd9cf2df76a0c3dc4cc9d1d781ea486
Vaivaswatha Nagaraj <vaivaswatha@zilliqa.com>
2021-04-19 14:28:59 +0530

    Build LLVM

--- scripts/build_llvm.sh
+++ scripts/build_llvm.sh
@@ -2,9 +2,12 @@
 llvm_commit="ea0e2ca1acb20781515c23850ec1ee7476909b2f"
 llvm_src="${HOME}"/llvm-project-"${llvm_commit}"
 llvm_build="${HOME}"/llvm_build
+
+eval "$(opam env)"
 mkdir -p "${llvm_build}"
 wget -nv https://github.com/llvm/llvm-project/archive/"${llvm_commit}".tar.gz
 tar -xzf ${llvm_commit}.tar.gz --directory="${HOME}"
 cd "$llvm_build" || exit 1
-eval "$(opam env)"
 cmake "${llvm_src}/llvm" -G "Ninja" -DCMAKE_BUILD_TYPE="Release" -DLLVM_ENABLE_ASSERTIONS=ON -DLLVM_TARGETS_TO_BUILD="host" -DLLVM_ENABLE_RTTI=ON -DLLVM_OCAML_INSTALL_PATH="~/.opam/4.12.0/lib"
+ninja bindings/ocaml/install
+
