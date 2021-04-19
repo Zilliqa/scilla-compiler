660f3feed9e7c9c6f352116aeae69308eeb41c07
Vaivaswatha Nagaraj <vaivaswatha@zilliqa.com>
2021-04-19 13:53:19 +0530

    explicitly install ocamlfind

--- scripts/build_llvm.sh
+++ scripts/build_llvm.sh
@@ -6,5 +6,5 @@ mkdir -p "${llvm_build}"
 wget -nv https://github.com/llvm/llvm-project/archive/"${llvm_commit}".tar.gz
 tar -xzf ${llvm_commit}.tar.gz --directory="${HOME}"
 cd "$llvm_build" || exit 1
-
+eval "$(opam env)"
 cmake "${llvm_src}/llvm" -G "Ninja" -DCMAKE_BUILD_TYPE="Release" -DLLVM_ENABLE_ASSERTIONS=ON -DLLVM_TARGETS_TO_BUILD="host" -DLLVM_ENABLE_RTTI=ON -DLLVM_OCAML_INSTALL_PATH="~/.opam/4.12.0/lib"
